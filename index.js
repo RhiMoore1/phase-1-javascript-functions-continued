90de7b402e2754d6dd0fd7d520cf60498cfb2575
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-03-13 13:49:39 -0700

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+function saturdayFun(eventFun = "roller-skate") {
+    return (`This Saturday, I want to ${eventFun}!`);
+}
+saturdayFun();
+
+const mondayWork = function (event = "go to the office") {
+    return (`This Monday, I will ${event}.`);
+}
+mondayWork();
+
+
+function wrapAdjective(stringName = "*") {
+    const innerFunction = function (value = 'special') {
+        return `You are ${stringName}${value}${stringName}!`;
+    }
+    return innerFunction;
+}
+wrapAdjective()();
